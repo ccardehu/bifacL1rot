be8aa09023c1b89d4f4abf2716118423cf0e21c6
Camilo Cardenas <c.a.cardenas-hurtado@lse.ac.uk>
2025-12-12 22:56:16 +0000

    First commit

--- R/main.R
+++ R/main.R
@@ -26,11 +26,20 @@
 #'
 #' @examples
 #' \dontrun{
-#' set.seed(123)
-#' A <- matrix(rnorm(50), 10, 5)
-#' result <- ALM(A)
-#' result$B
-#' result$Phi
+#' set.seed(1234)
+#' A <- matrix(0, 20, 4) # A 20 x 4 Factor loading matrix
+#' A[,1] = runif(20, 1, 2) # Bi-factor structure, first factor
+#' A[,2] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
+#' A[,3] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
+#' A[,4] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
+#'
+#' # Create rotation matrix (via random matrix):
+#' Ah =  array(rnorm(length(A), sd = .5), dim = dim(A))
+#' Tr = eigen(t(Ah) %*% Ah)$vectors
+#' Ah = (A)%*%(Tr)
+#' res = bifacL1rot::bifactorL1(Ah)
+#' Arot = res$B
+#' Phi = res$Phi
 #' }
 #'
 #' @export
--- README.md
+++ README.md
@@ -22,8 +22,8 @@ This is a basic example:
 ``` r
 set.seed(1234)
 A <- matrix(0, 20, 4) # A 20 x 4 Factor loading matrix
-A[,1] = runif(20, 1, 2) # Bi-factor structure, first factor
-A[,2] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
+A[,1] = runif(20, 1, 2) # Bi-factor structure, strong first-order factors
+A[,2] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25) # Sparse second-order factors
 A[,3] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
 A[,4] = runif(20, 0.5, 1) * rbinom(20, 1, 0.25)
 # Create rotation matrix (via random matrix):
